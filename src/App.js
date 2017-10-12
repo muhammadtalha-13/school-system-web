97c7fd0f7f6a053abb5b642cac3eb31505cf6aeb
Muhammad Talha <muhammadtalha-13@hotmail.com>
2017-10-12 21:14:40 +0500

    Testing Development Branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
                     <h1 className="App-title">Web App for School System</h1>
                 </header>
                 <p className="App-intro">
-                    Coming Soon
+                    Under Development - Coming Soon
                 </p>
             </div>
         );
