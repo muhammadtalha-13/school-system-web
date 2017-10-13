d9cf4f50864c293831d382dcac4918276abd7f74
Muhammad Talha <muhammadtalha-13@hotmail.com>
2017-10-13 18:10:03 +0500

    Added dependencies and connected with Firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger';
+
+import reducers from './reducers';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const middleware = applyMiddleware(logger);
+const store = createStore(reducers, middleware);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
